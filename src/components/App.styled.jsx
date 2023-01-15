a78efc6bef58666f57b49c849e6f7a4ffceb5ba3
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-15 17:44:38 +0200

    Create App.styled.jsx

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+margin: 20px;
+padding:20px;
+max-width: 350px;
+border-radius: 0px 0px 4px 4px;
+box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2);
+`
